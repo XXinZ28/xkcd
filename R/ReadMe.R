00e0f5eeb3401d6157d822411fa723eb389983db
Xinxin Zhang <tz88802@gmail.com>
2025-04-09 08:33:59 -0400

    Initial commit

---
+++ R/ReadMe.R
@@ -0,0 +1,7 @@
+# Description file is the heart of my R package
+# NAMESPACE file is equally important. Don't edit this file by hand, unless there are strange things to fix.
+# R folder is the key
+# xkcd.Rproj is just a RStudio project file
+# Rbuildignore tells what file to ignore: when I build the R package, pretend "^xkcd\.Rproj$^\.Rproj\.user$" don't exist.
+
+# set up git: usethis::use_git()
