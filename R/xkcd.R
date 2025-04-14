dde47c557852c108f78b426585785f227b00a4d4
Xinxin Zhang <tz88802@gmail.com>
2025-04-14 08:49:26 -0400

    add draw_comic function

--- R/xkcd.R
+++ R/xkcd.R
@@ -21,10 +21,44 @@
 #' dino_time <- xkcd(15)
 #' str(dino_time)
 #'
+#'
 xkcd <- function(number) {
   url <- file.path("https://xkcd.com", floor(number), "info.0.json")
   x <- jsonlite::read_json(url)
   return(x)
+}
+
+#' @title Plot an xkcd comic strip
+#' @description
+#' `draw_comic` display an xkcd comic strip as a plot in a graphics device (e.g., the Rstudio plot window).
+#' @param x  A list object as returned from [xkcd()]
+#' @importFrom tools file_ext
+#' @importFrom httr GET
+#' @importFrom png readPNG
+#' @importFrom jpeg readJPEG
+#' @importFrom graphics plot.new
+#' @importFrom grid grid.raster
+#' @returns A rastergrob grob. See "Details" section of [grid::grob()] for more information.
+#' @examples
+#' dino_time <- xkcd(15)
+#' draw_comic(dino_time)
+#' @export
+#'
+draw_comic <- function(x) {
+
+  img_type <- tools::file_ext(x$img)
+  # use file_ext to figure out the image file type.
+  # check environment by using rlang::fn_env(file_ext)
+  tmp <- httr::GET(url = x$img)
+
+  if (img_type == "png") {
+    img <- png::readPNG(tmp$content)
+  } else {
+    img <- jpeg::readJPEG(tmp$content)
+  }
+
+  graphics::plot.new()
+  grid::grid.raster(img)
 }
 
 # this function simulate the process of loading the package that I already installed.
